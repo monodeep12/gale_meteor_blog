d642018ae7651c3731ab8fdec76c1d90eef2e999
mono <monodeep12@gmail.com>
2015-05-31 13:47:10 +0530

    added the android app .apk file

---
+++ gale_meteor_blog/mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  name: 'Gale Meteor Blog',
+  description: 'An Android app built with Meteor',
+  version: '0.0.1'
+});
+
+App.icons({
+  'android_ldpi': 'public/resources/icons/icon-ldpi.png',
+  'android_mdpi': 'public/resources/icons/icon-mdpi.png',
+  'android_hdpi': 'public/resources/icons/icon-hdpi.png',
+  'android_xhdpi':'public/resources/icons/icon-xhdpi.png'
+});
